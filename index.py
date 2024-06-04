48fc6ba5632a5b783aac77e707f23a462c576830
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-05 00:47:55 +0800

    Update index.py

--- index.py
+++ index.py
@@ -16,12 +16,10 @@ try:
         unsafe_allow_html=True
     )
 
-    col = st.columns([1,2])
-    with col[0]:
-        st.title("Dashboard Agrimap Bali")
-    with col[1]:
-        st.write("Dashboard ini bertujuan untuk memetakan luas tanam atau panen padi berdasarkan hasil machine learning dan citra satelit.")
-        st.write("Peta heatmap di bawah akan menampilkan jenis fase tanaman padi yang terpilih. Terdapat juga data pendukung atau rekomendasi di bawah untuk melihat lebih detail potensi wilayah tersebut.")
+    # title
+    st.title("Dashboard Agrimap Bali")
+    st.write("Dashboard ini bertujuan untuk memetakan luas tanam atau panen padi berdasarkan hasil machine learning dan citra satelit.")
+    st.write("Peta heatmap di bawah akan menampilkan jenis fase tanaman padi yang terpilih. Terdapat juga data pendukung atau rekomendasi di bawah untuk melihat lebih detail potensi wilayah tersebut.")
 
     # load file path
     this_path = Path().resolve()
@@ -34,6 +32,10 @@ try:
     
     #kec_gdf = gpd.read_file(kec_path, driver='GeoJSON')
     kec_gdf = read_gpd(kec_path)
+    kec_gdf = kec_gdf.rename(columns={
+            'nmkec':'Kecamatan',
+            'nmdesa':'Desa',
+        })
     #kec_gdf = kec_gdf.to_crs("WGS84")
 
     # kab and its centroid
@@ -61,9 +63,9 @@ try:
         # PILIH kab
         selectbox_kab = st.selectbox("Pilih Kabupaten", options=list(kdkab.keys()), format_func=get_nmkab)
         # set pilihan kec based on pilihan kab
-        kec_choices = ["KUTA SELATAN"]#list( set(
-                #kec_gdf.loc[kec_gdf['nmkab'] == get_nmkab(selectbox_kab)].nmkec
-            #) )
+        kec_choices = list( set(
+                kec_gdf.loc[kec_gdf['nmkab'] == get_nmkab(selectbox_kab)].Kecamatan
+            ) )
         kec_choices.insert(0, "-")
         des_path = str(this_path) + f"/map_source/geo_desa_{selectbox_kab}.geojson"
         
@@ -74,8 +76,8 @@ try:
         # PILIH display choropleth
         #st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
         #t.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
-        opt_displaymap = ["Vegetatif 1","Vegetatif 2","Generatif","Persiapan Lahan"]
-        choose_displaymap=st.radio("Display map: ",opt_displaymap, index=2) #pilihan defaultnya Generatif
+        opt_displaymap = ["Luas Non Sawah", "Fase Persiapan Lahan","Fase Vegetatif 1","Fase Vegetatif 2","Fase Generatif",]
+        choose_displaymap=st.radio("Display map: ",opt_displaymap, index=4) #pilihan defaultnya Generatif
 
         # PILIH date file source
         from os import walk
@@ -120,10 +122,20 @@ try:
 
     # DATA FOR MAPPING
     titiktengah = False
+    # filter from date. read data luas padi satelit for mapping 
+    des_df = pd.read_csv(csv_path+f"/{selectbox_bln}_{selectbox_thn}.csv", dtype={'iddesa': object})
+    
+    # jika ga dipilih kab, maka tampilin aggregate
+    des_df['idkec'] = des_df['iddesa'].str[:7]
+    des_agg_df = des_df[['idkec','nonsawah','persiapan','vegetatif1','vegetatif2','generatif']].groupby("idkec").agg('mean')
+    if selectbox_kec == '-':
+        kec_gdf = kec_gdf.merge(des_agg_df, how='inner', on='idkec')
+    
+    # filter geomap desa (biar cuman ngeload ketika kefilter kab aja n ga mberatin)
     if Path(des_path).exists() :
         # koordinat centroid zoom map kec
         if selectbox_kec != '-':
-            titiktengah = kec_gdf[kec_gdf['nmkec'] == selectbox_kec ].geometry.centroid.iloc[0]
+            titiktengah = kec_gdf[kec_gdf['Kecamatan'] == selectbox_kec ].geometry.centroid.iloc[0]
         
         # read desa di kec terpilih
         #des_gdf = gpd.read_file(des_path, driver='GeoJSON')
@@ -131,27 +143,35 @@ try:
         if selectbox_kec != '-':
             des_gdf = des_gdf.loc[des_gdf['nmkec'] == selectbox_kec ]
         des_gdf['nmkab'] = get_nmkab(selectbox_kab)
-        #st.write(des_gdf.iddesa)
         
-        # filter display map and date
-        des_df = pd.read_csv(csv_path+f"/{selectbox_bln}_{selectbox_thn}.csv", dtype={'iddesa': object})
-        #des_dfmap = des_df.iloc[:,[0,opt_displaymap.index(choose_displaymap)+1]]
-        #st.write("Column choosen: ", des_df.columns)
+        # merge data hasil satelit dengan data shp geopandas
         des_df = des_gdf.merge(des_df, how='inner', on='iddesa')
         des_df['total'] = des_df['vegetatif1'] + des_df['vegetatif2'] + des_df['generatif'] + des_df['persiapan'] + des_df['nonsawah']
+        # rename column
         
         # data final for display map
         #kec_gdf = kec_gdf.drop('nmdesa', axis=1)
         #kec_gdf = kec_gdf.merge(des_gdf, how='outer')
-        kec_gdf = des_df
+        kec_gdf = des_df # desa df yang udah dimerge sama df dan geodf
+    kec_gdf[['nonsawah','persiapan','vegetatif1','vegetatif2','generatif']] = kec_gdf[['nonsawah','persiapan','vegetatif1','vegetatif2','generatif']].apply(lambda row: (row*100).round(3),axis = 1,result_type ='expand')
+    kec_gdf = kec_gdf.rename(columns={
+        'nmkec':'Kecamatan',
+        'nmdesa':'Desa',
+        'vegetatif1': 'Fase_Vegetatif1', 
+        'vegetatif2': 'Fase_Vegetatif2',
+        'generatif': 'Fase_Generatif',
+        'persiapan': 'Fase_Persiapan_lahan',
+        'nonsawah': 'Luas_Nonsawah'
+    })
+
 
     # MAIN VIEW MAPPING ============================
-    hover_data = [kec_gdf.nmkec, kec_gdf.nmdesa, kec_gdf.generatif, kec_gdf.vegetatif1, kec_gdf.vegetatif2, kec_gdf.persiapan] if selectbox_kab != '-' else [kec_gdf.nmkec]
+    hover_data = [kec_gdf.Kecamatan, kec_gdf.Desa, kec_gdf.Luas_Nonsawah, kec_gdf.Fase_Persiapan_lahan, kec_gdf.Fase_Generatif, kec_gdf.Fase_Vegetatif1, kec_gdf.Fase_Vegetatif2] if selectbox_kab != '-' else [kec_gdf.Kecamatan, kec_gdf.Luas_Nonsawah, kec_gdf.Fase_Persiapan_lahan, kec_gdf.Fase_Generatif, kec_gdf.Fase_Vegetatif1, kec_gdf.Fase_Vegetatif2]
     figmap = px.choropleth_mapbox(
                         kec_gdf,
                         geojson=kec_gdf.geometry,
                         locations=kec_gdf.index,
-                        color="luas" if selectbox_kab=='-' else des_df.columns[opt_displaymap.index(choose_displaymap)+8],
+                        color=kec_gdf.columns[opt_displaymap.index(choose_displaymap)+5] if selectbox_kab=='-' else kec_gdf.columns[opt_displaymap.index(choose_displaymap)+8],
                         color_continuous_scale=px.colors.sequential.Sunsetdark,
                         hover_name=kec_gdf.nmkab,
                         hover_data= hover_data
@@ -222,22 +242,30 @@ try:
 
         col3, col4 = st.columns([3,1])
         with col3: 
-            #st.write("df des col:", des_df.columns)
-            dfstack = des_df.iloc[:,[3,8,9,10,11]].set_index("nmdesa").stack().to_frame().reset_index()
-            dfstack.columns = ['Nama desa','Kategori','Persentase']
+            #st.write("df des col:", kec_gdf.columns)
+            if selectbox_kec == '-':
+                dfstack = kec_gdf.iloc[:,[2,8,9,10,11,12]].groupby("Kecamatan").agg('mean').stack().to_frame().reset_index()
+                dfstack.columns = ['Nama Kecamatan','Kategori','Persentase']
+            else:
+                dfstack = kec_gdf.iloc[:,[3,8,9,10,11,12]].set_index("Desa").stack().to_frame().reset_index()
+                dfstack.columns = ['Nama desa','Kategori','Persentase']
             figbar = px.bar(
                 dfstack,
-                y="Nama desa",
+                y="Nama Kecamatan" if selectbox_kec=='-' else "Nama desa",
                 x='Persentase',
                 color='Kategori',
-                color_discrete_sequence=px.colors.qualitative.Plotly_r
+                color_discrete_sequence=px.colors.qualitative.Plotly_r,
+            )
+            figbar.update_layout(legend=dict(
+                    orientation="h",
+                )                
             )
             st.plotly_chart(figbar)
         
         with col4:
             st.subheader("Rekomendasi / Data pendukung")
             if selectbox_kec == '-':
-                st.caption("KAB. "+str(selectbox_kab))
+                st.caption("KAB. "+str(get_nmkab(selectbox_kab)))
             else:
                 st.caption("KEC. "+str(selectbox_kec))
             st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis arcu, mollis sit amet orci nec, tincidunt eleifend tortor. In vehicula est eget enim eleifend, ac aliquam sem gravida. Suspendisse arcu lectus, ornare viverra mi eu, egestas placerat lectus.")
